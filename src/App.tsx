c24fac464867be9b28a406036beb089e7fb4dbc8
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-10-18 23:30:17 +0200

    seeds placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { useState } from 'react';
 import './App.css';
 
+import { Seed } from './components/Seed';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <div id="side">
-      <div id="left-side">
-        <div id="top-left">
-          top left
+      <div id="side">
+        <div id="left-side">
+          <div id="top-left">
+            <Seed />
+            <Seed />
+            <Seed />
+            <Seed />
+            <Seed />
+          </div>
+          <div id="bottom-left">bottom left</div>
         </div>
-        <div id="bottom-left">
-          bottom left
-        </div>
-      </div>
-      <div id="right-side">
-        right
-      </div>
+        <div id="right-side">right</div>
       </div>
     </>
   );
---
+++ src/components/Seed.tsx
@@ -0,0 +1,9 @@
+import seedsUrl from '../assets/seeds.png';
+
+export const Seed = () => (
+  <button className="selector">
+    <img src={seedsUrl} />
+    <br />
+    seed
+  </button>
+);
