b1a1368b148afcc01037dce07f35958923e4ef40
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-10-18 22:46:14 +0200

    remove button columns

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ function App() {
 
   return (
     <>
-      <div id="side">
-        <div id="left-side">
-          <div id="top-left">top left</div>
-          <div id="bottom-left">bottom left</div>
+    <div id="side">
+      <div id="left-side">
+        <div id="top-left">
+          top left
         </div>
-        <div id="right-side">
-          <div id="buttons-left">buttons left</div>
-          <div id="workaround-middle-buttons">
-          <div id="right-middle">right</div>
-          <div id="buttons-right">buttons right</div>
-          </div>
+        <div id="bottom-left">
+          bottom left
         </div>
+      </div>
+      <div id="right-side">
+        right
+      </div>
       </div>
     </>
   );
