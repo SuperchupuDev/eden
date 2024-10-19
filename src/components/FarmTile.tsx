2a1a751c1397004eefabc3323d3ea88b89adc96a
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-10-19 14:24:58 +0200

    basic tile modification logic

--- src/components/FarmTile.tsx
+++ src/components/FarmTile.tsx
@@ -1,5 +1,12 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { data } from '../data';
 import seedsUrl from '../assets/seeds.png';
+
+const states = [
+  { id: 0, url: '/grid-trash.png' },
+  { id: 1, url: '/grid-empty.png' }
+];
 
 interface FarmTileProps {
   id: number;
@@ -10,13 +17,39 @@ interface FarmTileProps {
   setFocused: (id: number | null) => void;
 }
 
-export const FarmTile = ({ id, seed, selected, setSelected, focused, setFocused }: FarmTileProps) => (
-  <button
-    className={`farm-selector ${selected ? 'selected' : ''}`}
-    onClick={() => setSelected(selected ? null : id)}
-    onMouseOver={() => setFocused(id)}
-    onMouseOut={() => (focused ? setFocused(null) : null)}
-  >
-    {focused ? <img id="tile-action" src={seed && data[seed - 1] ? (data[seed - 1].url ?? seedsUrl) : ''} /> : null}
-  </button>
-);
+export const FarmTile = ({ id, seed, selected, setSelected, focused, setFocused }: FarmTileProps) => {
+  const [state, setState] = useState(0);
+  const ref = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    ref.current?.style.setProperty('--tile-url', `url(${states[state].url})`);
+  }, [state]);
+
+  return (
+    <button
+      className={`farm-selector ${selected ? 'selected' : ''}`}
+      onClick={() => handleClick({ id, seed, selected, setSelected, state, setState })}
+      onMouseOver={() => setFocused(id)}
+      onMouseOut={() => (focused ? setFocused(null) : null)}
+      ref={ref}
+    >
+      {focused ? <img id="tile-action" src={seed && data[seed - 1] ? (data[seed - 1].url ?? seedsUrl) : ''} /> : null}
+    </button>
+  );
+};
+
+interface ClickProps {
+  id: number;
+  seed: number | null;
+  selected: boolean;
+  setSelected: (id: number | null) => void;
+  state: number;
+  setState: (state: number) => void;
+}
+
+function handleClick({ id, seed, selected, setSelected, state, setState }: ClickProps) {
+  if (seed === 10 && state === 0) {
+    setState(1);
+  }
+  setSelected(selected ? null : id);
+}
